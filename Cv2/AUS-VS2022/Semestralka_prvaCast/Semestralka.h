6cc4fd9830ea9fec7a21e39df2b8777c95178434
Benjamin Vyhnal <benjamin.vyhnal@gmail.com>
2025-04-15 15:04:11 +0200

    Nefunkcna 1 cast ;)

---
+++ Cv2/AUS-VS2022/Semestralka_prvaCast/Semestralka.h
@@ -0,0 +1,25 @@
+#pragma once  
+#include <vector>  
+#include "Vrchol.h"  
+#include "libds/amt/explicit_hierarchy.h"  
+#include "Zastavka.h"  
+
+using ViacCestnaHierarchia = ds::amt::MultiWayExplicitHierarchy<Vrchol>;  
+using BlokHierarchie = ds::amt::MultiWayExplicitHierarchyBlock<Vrchol>;  
+
+class Semestralka  
+{  
+   ViacCestnaHierarchia hierarchiaZastavok;  
+   BlokHierarchie* koren_;  
+   std::vector<Zastavka> zastavky;  
+   std::vector<Zastavka> filtrovaneZastavky;  
+
+public:  
+   Semestralka(std::vector<Zastavka>& zastavky, std::vector<Zastavka>& filtrovaneZastavky)  
+       : zastavky(zastavky), filtrovaneZastavky(filtrovaneZastavky), koren_(nullptr) {}  
+
+   void nacitajVsetkyZastavky(std::string nazovSuboru);  
+   void zobrazMenuPrvaCast();  
+   void zobrazMenuDruhaCast();  
+   void vypisVsetkyZastavky(const std::vector<Zastavka>& zastavkyNaVypis);  
+};
